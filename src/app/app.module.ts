12fe9f43b2a7dbf250b4149f601f52eac2385f26
codealfred <munozrodas.luisalfredo@gmail.com>
2022-02-08 13:40:38 -0500

    iniciando el proyecto de prueba, con termux

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Home1Component } from './home1/home1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Home1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
